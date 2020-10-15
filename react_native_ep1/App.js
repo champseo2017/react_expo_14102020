d2bdf522ed0ebd58350222a35068966cfa58b401
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-10-16 01:37:34 +0700

    React Native Tutorial #4 - Using State
    React Native Tutorial #4 - Using State

--- react_native_ep1/App.js
+++ react_native_ep1/App.js
@@ -1,42 +1,46 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+//import liraries
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-export default function App() {
+// create a component
+const App = () => {
+  const [name, setName] = useState("shaun");
+  const [person, setPerson] = useState({
+    name: "mario",
+    age: 40,
+  });
+  const clickHandler = () => {
+    setName("chun-li");
+    setPerson({
+      name: 'luigi',
+      age: 45
+    })
+  };
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello, World</Text>
-      </View>
-      <View style={styles.body}>
-        <Text style={styles.boldText}>
-          Lorem ipsum <Text>test</Text> dolor sit amet.
-        </Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
+      <Text>My name is {name}</Text>
+      <Text>
+        His name is {person.name} and his age {person.age}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button title="update state" onPress={clickHandler} />
       </View>
     </View>
   );
-}
+};
 
+// define your styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#2c3e50",
   },
-  header: {
-    backgroundColor: "pink",
-    padding: 20,
-  },
-  boldText: {
-    fontWeight: "bold",
-  },
-  body: {
-    backgroundColor: "yellow",
-    padding: 20,
-    fontWeight: "bold",
+  buttonContainer: {
+    marginTop: 20,
   },
 });
+
+//make this component available to the app
+export default App;
