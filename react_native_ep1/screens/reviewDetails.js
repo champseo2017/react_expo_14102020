7576167c34286dbee6dc7680657cf7eb46c8789d
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2020-10-23 15:47:23 +0700

    React Native Tutorial #27 - Using Images
    React Native Tutorial #27 - Using Images

--- react_native_ep1/screens/reviewDetails.js
+++ react_native_ep1/screens/reviewDetails.js
@@ -1,22 +1,39 @@
 //import liraries
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { globalStyles } from "../styles/global";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { globalStyles, images } from "../styles/global";
 import { Button } from "native-base";
 import Card from "../shared/card";
 
 // create a component
 const ReviewDetails = ({ navigation }) => {
+  const rating = navigation.getParam('rating')
   return (
     <View style={globalStyles.container}>
       <Card>
-      <Text>{navigation.getParam("title")}</Text>
-      <Text>{navigation.getParam("body")}</Text>
-      <Text>{navigation.getParam("rating")}</Text>
+        <Text>{navigation.getParam("title")}</Text>
+        <Text>{navigation.getParam("body")}</Text>
+        <View style={styles.rating}>
+          <Text>GameZone rating:</Text>
+          <Image
+            source={images.ratings[rating]}
+          />
+        </View>
       </Card>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee'
+  }
+});
 
 //make this component available to the app
 export default ReviewDetails;
